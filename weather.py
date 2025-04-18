3a83d57cb413f78d1388f1283003b732b2c2afc4
programmmerjosh <programmmerjosh@gmail.com>
2025-04-18 19:29:26 +0100

    idea for TODOs

--- weather.py
+++ weather.py
@@ -25,6 +25,7 @@ def main():
     # TODO: find a job for OpenAI API
     # TODO: add more weather data sources
     # TODO: test forcast accuracy new logic
+    # TODO: think about ways to use the cloud cover summary ~ perhaps a table where we show a counter for each category for mornings, afternoons and nights
     # TODO: Deploy the app and make sure `python weather.py` is called automatically daily
 
     with open("data/locations.json") as f:
