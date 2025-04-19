15e2d23b086a8042aa3ca1804a62d94694547ce2
programmmerjosh <programmmerjosh@gmail.com>
2025-04-19 16:55:37 +0100

    move todos to a new file in logs

--- weather.py
+++ weather.py
@@ -22,12 +22,6 @@ def main():
     d_in_five_days = get_forecast_date(5)
     target_dates = {today, d_in_three_days, d_in_five_days}
 
-    # TODO: find a job for OpenAI API
-    # TODO: add more weather data sources
-    # TODO: test forcast accuracy new logic
-    # TODO: think about ways to use the cloud cover summary ~ perhaps a table where we show a counter for each category for mornings, afternoons and nights
-    # TODO: deploy the app and make sure `python weather.py` is called automatically daily
-
     with open("data/locations.json") as f:
         LOCATIONS = json.load(f)
 
